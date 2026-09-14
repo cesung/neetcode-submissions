0153070895d6e3a0f99b011fb41bd4ffe82ba8e5
Cheng-En Sung <76089914+cesung@users.noreply.github.com>
2026-09-13 20:31:20 -0700

    Add: design-twitter-feed - submission-4

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-4.py
@@ -0,0 +1,33 @@
+class Twitter:
+
+    def __init__(self):
+        self.relationship = defaultdict(set)
+        self.tweet = defaultdict(list)
+        self.clock = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweet[userId].append( (tweetId, self.clock) )
+        self.clock += 1
+
+    def _getNewsFeed(self, userId, vis):
+        if userId in vis:
+            return []
+        
+        vis.add(userId)
+        tweets = self.tweet[userId][:]
+        for followeeId in self.relationship[userId]:
+            tweets.extend(self._getNewsFeed(followeeId, vis))
+        
+        return tweets
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        tweets = self._getNewsFeed(userId, set())
+        tweets.sort(key = lambda x : x[1], reverse=True)
+        return [tweet_id for tweet_id, clock in tweets[:10]]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.relationship[followerId].add(followeeId)
+        
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.relationship[followerId].discard(followeeId)
+        
